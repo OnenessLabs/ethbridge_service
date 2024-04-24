a36aac29eeb3f914b1992bf618a738b7a2c04906
yonezu60 <zzz@4F0qM.com>
2024-04-24 18:53:47 +0800

    remove fetal

--- src/cp/index.go
+++ src/cp/index.go
@@ -184,7 +184,7 @@ func goDotEnvVariable(key string) string {
 	err := godotenv.Load(".env")
 
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		log.Printf("Error loading .env file")
 	}
 
 	return os.Getenv(key)
